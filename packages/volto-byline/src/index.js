8637580986c83e6ea1e39ccbb3214a841eafe376
Fosten <fosten@gmail.com>
2024-10-27 12:15:41 -0400

    Add Byline block and Listing customizations

--- packages/volto-byline/src/index.js
+++ packages/volto-byline/src/index.js
@@ -1,4 +1,21 @@
+// All your imports required for the config here BEFORE this line
+import '@plone/volto/config';
+import customBlocks from './components/Blocks/customBlocks';
+
 const applyConfig = (config) => {
+  // Add here your project's configuration here by modifying `config` accordingly
+  config.blocks = {
+    ...config.blocks,
+    initialBlocks: {
+      'News Item': ['title', 'description', 'byline', 'leadimage', 'slate'],
+    },
+    blocksConfig: {
+      ...config.blocks.blocksConfig,
+      ...config.blocks.initialBlocks,
+      ...customBlocks,
+    },
+  };
+
   return config;
 };
 
